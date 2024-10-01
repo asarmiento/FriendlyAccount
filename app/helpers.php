922b995ed3faa107e90a3edc2a7fa4937aec895f
Anwar Sarmiento Ramos <anwarsarmiento@gmail.com>
2024-10-01 06:00:44 -0400

    fix
    prueba de log

--- app/helpers.php
+++ app/helpers.php
@@ -46,6 +46,7 @@ function actionList(){
 
 function changeLetterMonth($month){
     $very = explode(0,$month);
+    \Log::info("Camvio ".json_encode($very)." cambio ".$month);
     if(strlen($month)== 2 || $very[0]== '' || $very[0]== 0 ):
         $months= ['01'=>'Enero','02'=>'Febrero','03'=>'Marzo','04'=>'Abril','05'=>'Mayo','06'=>'Junio','07'=>'Julio','08'=>'Agosto','09'=>'Septiembre','10'=>'Octubre','11'=>'Noviembre','12'=>'Diciembre'];
     else:
